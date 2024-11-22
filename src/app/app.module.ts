7e566158b280cff4b1b6bb955f46eebe5990177c
AJAO.H <hazzanaj@gmail.com>
2024-11-22 11:27:21 +0200

    Added Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
