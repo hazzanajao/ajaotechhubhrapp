2d20b6ef8106554d6131671e05d359d4ca272833
AJAO.H <hazzanaj@gmail.com>
2024-11-22 11:34:23 +0200

    Added Contact-form route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContactFormComponent} from "./contact-form/contact-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/contact-form', pathMatch: 'full' },
+  { path: 'contact-form', component :ContactFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
