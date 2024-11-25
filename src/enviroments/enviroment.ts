6b6f99a9decbd0fb77831e99db6920d043af83b5
AJAO.H <hazzanaj@gmail.com>
2024-11-25 14:08:28 +0200

    Angular Contact Form Completed

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
+};
